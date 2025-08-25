425ae6d745cae60d8e430b9580f2ea33ad4af130
tr-Kalyan <kalyansde1@gmail.com>
2025-08-25 23:56:51 +0530

    bug fix: ERC20 removed from lottery.sol

--- script/DeployLotteryFactory.s.sol
+++ script/DeployLotteryFactory.s.sol
@@ -10,10 +10,10 @@ contract DeployLotteryFactory is Script {
     function run() external returns (LotteryFactory factory) {
 
         uint256 pk = vm.envUint("PRIVATE_KEY"); // deployer key
-        uint64 subId = uint64(vm.envUint("VRF_SUBSCRIPTION_ID")); // VRF sub (uint64)
+        uint64 subId = uint64(vm.envUint("SUBSCRIPTION_ID")); // VRF sub (uint64)
         address vrf = vm.envAddress("VRF_COORDINATOR"); // coordinator
         address link = vm.envAddress("LINK_TOKEN"); // LINK token
-        bytes32 gasLane = vm.envBytes32("VRF_KEYHASH"); // keyHash
+        bytes32 gasLane = vm.envBytes32("KEY_HASH"); // keyHash
         
         vm.startBroadcast(pk);
         factory = new LotteryFactory(subId, vrf, link, gasLane);
